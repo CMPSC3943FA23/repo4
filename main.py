43f5c910dd782e125a1910ab0ac04668bbbbd19e
Sumit Basnet <103460051+Sumit363@users.noreply.github.com>
2023-10-03 11:10:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,23 @@ BG = "#B1DDC6"
 current_card = {}
 to_learn = {}
 engine = pyttsx3.init()
+
+
+
+
+
+
+
+
+
+
+
+
+
+window = Tk()
+window.title("FlipCard")
+window.config(padx=50, pady=50, bg=BG) #padding: 50px to x and y
+
+flip_timer = window.after(3000, func=flip_card) #Flips the card in 3 secs
+
+canvas = Canvas(width=800, height=526) #This is our actual width and height of the app window.
